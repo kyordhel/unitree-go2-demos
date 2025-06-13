2035c039f8b055e831ec2cbf69cb1dd35ad862a7
Mauricio Matamoros <kyordhel@gmail.com>
2025-06-13 01:43:14 -0600

    Add walk demo

--- walk_demo/walk.cpp
+++ walk_demo/walk.cpp
@@ -9,6 +9,8 @@
 #include <chrono>
 #include <memory>
 #include <thread>
+#include <cstdint>
+#include <algorithm>
 #include <rclcpp/rclcpp.hpp>
 
 #include "sport_client/sport_client.h"
@@ -17,6 +19,8 @@ using Request = unitree_api::msg::Request;
 
 rclcpp::Publisher<Request>::SharedPtr publisher;
 void task();
+bool walk(SportClient& sc, float speed, uint8_t seconds);
+bool turn(SportClient& sc, float arad);
 
 int main(int argc, char **argv){
 	rclcpp::init(argc, argv);
@@ -32,16 +36,6 @@ int main(int argc, char **argv){
 void task(){
 	SportClient sc(publisher);
 
-	printf("Dog stand\n");
-	sc.StandUp();
-	std::this_thread::sleep_for(std::chrono::seconds(2));
-	if( !rclcpp::ok() ) return;
-
-	printf("Dog sit\n");
-	sc.Sit();
-	std::this_thread::sleep_for(std::chrono::seconds(2));
-	if( !rclcpp::ok() ) return;
-
 	printf("Dog down\n");
 	sc.StandDown();
 	std::this_thread::sleep_for(std::chrono::seconds(2));
@@ -52,8 +46,39 @@ void task(){
 	std::this_thread::sleep_for(std::chrono::seconds(2));
 	if( !rclcpp::ok() ) return;
 
-	printf("Dog down\n");
-	sc.StandDown();
-	std::this_thread::sleep_for(std::chrono::seconds(2));
+	sc.SpeedLevel(-1);
 
+	printf("Dog move forward\n");
+	if( ! walk(sc, 0.3, 5) ) return;
+	
+	printf("Dog move backwards\n");
+	if( ! walk(sc, -0.3, 5) ) return;
+
+	printf("Turn and sleep\n");
+	if( ! turn(sc, -1) ) return;
+	if( ! turn(sc,  2) ) return;
+	if( ! turn(sc, -1) ) return;
+	sc.StandDown();
+}
+
+bool walk(SportClient& sc, float speed, uint8_t seconds){
+	if(speed >  1) speed =  1;
+	if(speed < -1) speed = -1;
+	for(uint8_t s = 0; s < seconds; ++s){
+		if( !rclcpp::ok() ) return false;
+		sc.Move(speed, 0, 0);
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
+	return true;
+}
+
+bool turn(SportClient& sc, float arad){
+	if(arad >  3.141592) arad =  3.141592;
+	if(arad < -3.141592) arad = -3.141592;
+	uint16_t time = std::min<uint16_t>(500, 1000 * std::abs(arad));
+
+	if( !rclcpp::ok() ) return false;
+	sc.Move(0, 0, arad);
+	std::this_thread::sleep_for(std::chrono::milliseconds(time));
+	return true;
 }

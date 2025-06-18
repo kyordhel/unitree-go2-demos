dd939999b09d7b9b51c51c0163505b25cb3996b6
Mauricio Matamoros <kyordhel@gmail.com>
2025-06-18 01:21:13 -0600

    temp commit

--- dog_base/dogbase.cpp
+++ dog_base/dogbase.cpp
@@ -17,6 +17,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
+#include <nav_msgs/msg/odometry.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 #include <geometry_msgs/msg/transform_stamped.hpp>
 
@@ -30,6 +31,8 @@ using Request          = unitree_api::msg::Request;
 using Twist            = geometry_msgs::msg::Twist;
 using TwistPtr         = std::shared_ptr<Twist>;
 using TransformStamped = geometry_msgs::msg::TransformStamped;
+using Odometry         = nav_msgs::msg::Odometry;
+using OdometryPtr      = std::shared_ptr<Odometry>;
 using SportClientPtr   = std::shared_ptr<SportClient>;
 
 
@@ -38,16 +41,22 @@ int main(int argc, char **argv);
 class DogBaseNode : public rclcpp::Node{
 	private:
 		SportClientPtr sc;
-		rclcpp::Publisher<Request>::SharedPtr pub;
+		rclcpp::Publisher<Request>::SharedPtr pub_api;
+		rclcpp::Publisher<Odometry>::SharedPtr pub_odom;
 		rclcpp::Subscription<Twist>::SharedPtr sub_cmd_vel;
 		// rclcpp::Subscription<Response>::SharedPtr sub_response;
 		std::unique_ptr<tf2_ros::TransformBroadcaster> tbc;
+		ros::Time start;
 
 	public:
 		DogBaseNode();
 
 	private:
 		void handleTwist(const TwistPtr msg);
+		void publishOdometry(const TwistPtr msg, const tf2::Quaternion& q);
+		void broadcastTransform(const TwistPtr msg, const tf2::Quaternion& q);
+		std::chrono::duration<double> getElapsed();
+		 // = std::chrono::high_resolution_clock::now();
 		// void handleResponse(const ResponsePtr msg);
 };
 
@@ -64,15 +73,18 @@ int main(int argc, char **argv){
 
 DogBaseNode::DogBaseNode():
 	Node("dog_base_node"){
+	using namespace std::literals;
+	start = std::chrono::system_clock::now() + 24h;
 	tbc = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
-	pub = this->create_publisher<Request>("/api/sport/request", 10);
+	pub_api = this->create_publisher<Request>("/api/sport/request", 10);
+	pub_odom = this->create_publisher<Odometry>("/odom", 10);
 	// sub_response = this->create_subscription<Response>("/api/sport/response", 10,
 	//	std::bind(&DogBaseNode::handleResponse, this, std::placeholders::_1)
 	//);
 	sub_cmd_vel = this->create_subscription<Twist>("/cmd_vel", 10,
 		std::bind(&DogBaseNode::handleTwist, this, std::placeholders::_1)
 	);
-	sc  = std::make_shared<SportClient>(pub);
+	sc  = std::make_shared<SportClient>(pub_api);
 
 	RCLCPP_INFO(this->get_logger(), "Dogbase node running. Standing up...");
 	sc->StandDown();
@@ -84,13 +96,36 @@ DogBaseNode::DogBaseNode():
 }
 
 
-
 void DogBaseNode::handleTwist(const TwistPtr msg){
-	RCLCPP_INFO(this->get_logger(),
-		"Handle twist: (%0.2f,%0.2f,%0.2f) (%0.2f,%0.2f,%0.2f)",
-		msg->linear.x,  msg->linear.y,  msg->linear.z,
-		msg->angular.x, msg->angular.y, msg->angular.z
-	);
+	auto elapsed = getElapsed();
+	// RCLCPP_INFO(this->get_logger(),
+	// 	"Handle twist: (%0.2f,%0.2f,%0.2f) (%0.2f,%0.2f,%0.2f)",
+	// 	msg->linear.x,  msg->linear.y,  msg->linear.z,
+	// 	msg->angular.x, msg->angular.y, msg->angular.z
+	// );
+
+	// Go2 moves on ground: x is front-back, y is left-right
+	// Here we forward the Twist data to the Go2 and tranform.
+	sc->Move(msg->linear.x, msg->linear.y, msg->angular.z);
+	// Rotation is only on theta (vector z)
+	tf2::Quaternion q;
+	q.setRPY(0, 0, msg->angular.z);
+	broadcastTransform(msg, q);
+	publishOdometry(msg, q);
+}
+
+
+void DogBaseNode::publishOdometry(const TwistPtr msg, const tf2::Quaternion& q){
+	Odometry o;
+
+	// Initialize o variables
+	o.header.stamp = this->get_clock()->now();
+	o.header.frame_id = "odom";
+	o.child_frame_id = "base_link";
+}
+
+
+void DogBaseNode::broadcastTransform(const TwistPtr msg, const tf2::Quaternion& q){
 	TransformStamped t;
 
 	// Initialize tf variables
@@ -98,17 +133,11 @@ void DogBaseNode::handleTwist(const TwistPtr msg){
 	t.header.frame_id = "odom";
 	t.child_frame_id = "base_link";
 
-	// Go2 moves on ground: x is front-back, y is left-right
-	// Here we forward the Twist data to the Go2 and tranform.
-	sc->Move(msg->linear.x, msg->linear.y, msg->angular.z);
 	t.transform.translation.x = msg->linear.x;
 	t.transform.translation.y = msg->linear.y;
 	t.transform.translation.z = 0.0;
 	//t.transform.translation = msg->linear;
 
-	// Rotation is only on theta (vector z)
-	tf2::Quaternion q;
-	q.setRPY(0, 0, msg->angular.z);
 	t.transform.rotation.x = q.x();
 	t.transform.rotation.y = q.y();
 	t.transform.rotation.z = q.z();
@@ -118,6 +147,13 @@ void DogBaseNode::handleTwist(const TwistPtr msg){
 	tbc->sendTransform(t);
 }
 
+
+std::chrono::duration<double> DogBaseNode::getElapsed(){
+	auto end = std::chrono::system_clock::now();
+	std::chrono::duration<double> elapsed = end - start;
+	start = end;
+	return elapsed;
+}
 
 //void DogBaseNode::handleResponse(const ResponsePtr msg){
 //
